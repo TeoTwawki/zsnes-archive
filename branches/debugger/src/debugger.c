d5340bab2dc03b8ba101c33f3eba690408e9f9de
Samuel Bronson <Samuel Bronson@8cdf4848-9e13-0410-98e6-c9478ff1fb58>
2006-06-26 02:02:22 +0000

    SPC debugging -- addressing modes were a lot quicker to implement than the 65816 ones, though the tables weren't much fun to copy. I would like someone to double check the opcode name table, though, because I typed that one out by hand...
    git-svn-id: https://svn.zsnes.com/rep/zsnes@3370 8cdf4848-9e13-0410-98e6-c9478ff1fb58

--- branches/debugger/src/debugger.c
+++ branches/debugger/src/debugger.c
@@ -12,6 +12,8 @@
 
 // All of these should be in headers, people!
 
+extern unsigned char oamram[1024], SPCRAM[65472], DSPMem[256];
+
 extern unsigned char curblank;
 extern unsigned char curcyc;
 extern unsigned char curypos;
@@ -29,14 +31,26 @@ extern char *dmadata;
 
 extern void (*memtabler8[256])();
 
+
+// SPC stuff
+
+extern unsigned char *spcPCRam;
+extern unsigned char spcA, spcX, spcY, spcS, spcNZ, spcP;
+
+
 extern unsigned char memtabler8_wrapper(unsigned char, unsigned short);
 extern void breakops_wrapper(unsigned char, unsigned short);
 
 extern void regaccessbankr8();
 extern void start65816();
 
+
 char *ocname;
 unsigned char addrmode[];
+char *spcnametab[];
+char *AddressTable[];
+unsigned char ArgumentTable[];
+
 
 /*
 unsigned short debugh  = 0; // debug head
@@ -176,6 +190,10 @@ void debugloop() {
 	nextopcode();
     if (!(debugds & 1))
 	nextspcopcode();
+
+    // redrawing the display is always a good idea
+    wrefresh(debugwin);
+    refresh();
   
   b:
    key = getch();
@@ -233,7 +251,7 @@ void debugloop() {
    skipdebugsa1 = 0;
    asm_call(execnextop);
    skipdebugsa1 = 1;
-   if (soundon && (debugds & 2) && (cycpbl <= 55))
+   if (soundon && (debugds & 2) && (cycpbl >= 55))
        goto e;
    goto a;
 	
@@ -589,10 +607,6 @@ void nextopcode() {
 	out65816();
     //else 
     //	outputbuffersa1();
-
-    // redrawing the display is always a good idea
-    wrefresh(debugwin);
-    refresh();
 }
 
 
@@ -604,14 +618,144 @@ void cleardisplay() {
 // Next SPC Opcode          Writes the next opcode & regs
 //*******************************************************
 // 008000 STZ $123456,x A:0000 X:0000 Y:0000 S:01FF DB:00 D:0000 P:33 E+
-void nextspcopcode() {
-    // ...
+
+void outspc_addrmode() {
+    unsigned char mode;
+    char *format;
+    char buf[16] = "               ";
+    char *p;
+
+#define HEX8(val)   do { p += sprintf(p, "%02x", val); *p = ' '; } while (0) 
+#define HEX16(val)  do { p += sprintf(p, "%04x", val); *p = ' '; } while (0)
+
+    mode = ArgumentTable[spcPCRam[0]];
+    format = AddressTable[mode];
+    
+    // memset(buf, ' ', 15); buf[15] = 0;
+    
+    p = buf;
+    while (*format) {
+	if (*format != '%') {
+	    *p++ = *format++;
+	    continue;
+	}
+	
+	format++;
+	switch (*format++) {
+
+	case '1': // first byte
+	    HEX8(spcPCRam[1]);
+	    break;
+
+	case '2': // second byte
+	    HEX8(spcPCRam[2]);
+	    break;
+
+	case '3': // hinib
+	    *p++ = (spcPCRam[0] >> 4) + '0';
+	    *p++ = ' ';
+	    break;
+
+	case '4': // hinib2
+	    *p++ = (spcPCRam[0] >> 5) + '0';
+	    *p++ = ' ';
+	    break;
+	    
+	case '5': // rela2pc2
+	{   
+	    signed char off;
+	    off = *(signed char*)(spcPCRam+1);
+	    HEX16(off + 2 + (spcPCRam - SPCRAM));
+	    // format += 3;
+	    break;
+	}
+
+	case '6': // dp
+	{
+	    *p++ = '$';
+
+	    if (spcP & 0x20) {
+		*p++ = '1';
+	    } else {
+		*p++ = '0';
+	    }
+
+	    break;	    
+	}
+	    
+	case '8': // memorybit
+	    HEX16((*(unsigned short*)(spcPCRam+1)) >> 3);
+	    // format += 2;
+	    break;
+	    
+	case '9': // memorybitlow
+	    *p++ = ',';
+	    *p++ = (spcPCRam[1] & 0x7) + '0';
+	    break;
+	    
+	case 'A': // rela2pc1
+	{
+	    signed char off;
+	    off = *(signed char*)(spcPCRam+1);
+	    HEX16(off + 2 + spcPCRam - SPCRAM);
+	    // format += 2;
+	    break;
+	}
+
+	case 'B': // rela2pc2at2
+	{
+	    signed char off;
+	    off = *(signed char*)(spcPCRam+2);
+	    HEX16(off + 2 + spcPCRam - SPCRAM);
+	    // format += 2;
+	    break;
+	}
+
+	}
+		
+    }
+    
+    buf[15] = 0;
+    waddstr(debugwin, buf);
 }
+
+void nextspcopcode() {
+    if (!soundon)
+	return;
+    if (cycpbl >= 55)
+	return;
+    
+    // output spc pc & opcode #
+    wprintw(debugwin, " %04x/%02x ", spcPCRam - SPCRAM, spcPCRam[0]);
+
+    
+    // output instruction
+    wprintw(debugwin, "%-6s", spcnametab[spcPCRam[0]]);
+    outspc_addrmode();
+
+    // output registers
+    wprintw(debugwin, "A:%02x X:%02x Y:%02x S:%02x ", spcA, spcX, spcY, spcS);
+    wprintw(debugwin, "N%cO%cD%c?%cH%cI%cZ%cC%c",
+	    (spcNZ & 0x80) ? '+' : '-',
+	    (spcP  & 0x40) ? '+' : '-',
+	    (spcP  & 0x20) ? '+' : '-',
+	    (spcP  & 0x10) ? '+' : '-',
+	    (spcP  & 0x08) ? '+' : '-',
+	    (spcP  & 0x04) ? '+' : '-',
+	    (spcP  & 0x02) ? '+' : '-',
+	    (spcP  & 0x01) ? '+' : '-');
+
+    wprintw(debugwin, "\n");
+    
+}
+
 
 //*******************************************************
 // Debugger OpCode Information
 //*******************************************************
 
+// Yes, I know, not very C style, but I really really really didn't
+// want to type all those quote marks and commas. --SamB
 char *ocname =
 "BRK ORA COP ORA TSB ORA ASL ORA PHP ORA ASL PHD TSB ORA ASL ORA "
 "BPL ORA ORA ORA TRB ORA ASL ORA CLC ORA INC TCS TRB ORA ASL ORA "
@@ -656,7 +800,226 @@ unsigned char addrmode[256] = {
     15, 7,18,23, 2,10,10, 8, 6,13,21, 6,20,12,12,14
 };
 
-extern unsigned char oamram[1024], SPCRAM[65472], DSPMem[256];
+
+char *spcnametab[256] = {
+    "NOP",  "TCALL", "SET1",  "BBS",
+    "OR",   "OR",    "OR",    "OR",
+    "OR",   "OR",    "OR1",   "ASL",
+    "ASL",  "PUSH",  "TSET1", "BRK",
+    
+    "BPL",  "TCALL", "CLR1",  "BBC",
+    "OR",   "OR",    "OR",    "OR",
+    "OR",   "OR",    "DECW",  "ASL",
+    "ASL",  "DEC",   "CMP",   "JMP",
+    
+    "CLRP", "TCALL", "SET1",  "BBS",
+    "AND",  "AND",   "AND",   "AND",
+    "AND",  "AND",   "OR1",   "ROL",
+    "ROL",  "PUSH",  "CBNE",  "BRA",
+    
+    "BMI",  "TCALL", "CLR1",  "BBC",
+    "AND",  "AND",   "AND",   "AND",
+    "AND",  "AND",   "INCW",  "ROL",
+    "ROL",  "INC",   "CMP",   "CALL",
+
+    
+    "SETP", "TCALL", "SET1",  "BBS",
+    "EOR",  "EOR",   "EOR",   "EOR",
+    "EOR",  "EOR",   "AND1",  "LSR",
+    "LSR",  "PUSH",  "TCLR1", "PCALL",
+
+    "BVC",  "TCALL", "CLR1",  "BBC",
+    "EOR",  "EOR",   "EOR",   "EOR",
+    "EOR",  "EOR",   "CMPW",  "LSR",
+    "LSR",  "MOV",   "CMP",   "JMP",
+    
+    "CLRC", "TCALL", "SET1",  "BBS",
+    "CMP",  "CMP",   "CMP",   "CMP",
+    "CMP",  "CMP",   "AND1",  "ROR",
+    "ROR",  "PUSH",  "DMNZ",  "RET",
+    
+    "BVS",  "TCALL", "CLR1",  "BBC",
+    "CMP",  "CMP",   "CMP",   "CMP",
+    "CMP",  "CMP",   "ADDW",  "ROR",
+    "ROR",  "MOV",   "CMP",   "RET1",
+
+
+    "SETC", "TCALL", "SET1",  "BBS",
+    "ADC",  "ADC",   "ADC",   "ADC",
+    "ADC",  "ADC",   "EOR1",  "DEC",
+    "DEC",  "MOV",   "POP",   "MOV",
+
+    "BCC",  "TCALL", "CLR1",  "BBC",
+    "ADC",  "ADC",   "ADC",   "ADC",
+    "ADC",  "ADC",   "SUBW",  "DEC",
+    "DEC",  "MOV",   "DIV",   "XCN",
+
+    "EI",   "TCALL", "SET1",  "BBS",
+    "SBC",  "SBC",   "SBC",   "SBC",
+    "SBC",  "SBC",   "MOV1",  "INC",
+    "INC",  "CMP",   "POP",   "MOV",
+    
+    "BCS",  "TCALL", "CLR1",  "BBC",
+    "SBC",  "SBC",   "SBC",   "SBC",
+    "SBC",  "SBC",   "MOVW",  "INC",
+    "INC",  "MOV",   "DAS",   "MOV",
+
+
+    "DI",   "TCALL", "SET1",  "BBS",
+    "MOV",  "MOV",   "MOV",   "MOV",
+    "CMP",  "MOV",   "MOV1",  "MOV",
+    "MOV",  "MOV",   "POP",   "MUL",
+    
+    "BNE",  "TCALL", "CLR1",  "BBC",
+    "MOV",  "MOV",   "MOV",   "MOV",
+    "MOV",  "MOV",   "MOVW",  "MOV",
+    "DEC",  "MOV",   "CBNE",  "DAA",
+    
+    "CLRV", "TCALL", "SET1",  "BBS",
+    "MOV",  "MOV",   "MOV",   "MOV",
+    "MOV",  "MOV",   "NOT1",  "MOV",
+    "MOV",  "NOTC",  "POP",   "SLEEP",
+    
+    "BEQ",  "TCALL", "CLR1",  "BBC",
+    "MOV",  "MOV",   "MOV",   "MOV",
+    "MOV",  "MOV",   "MOV",   "MOV",
+    "INC",  "MOV",   "DBNZ",  "STOP"
+};
+// need... air... badly...!
+
+
+// %1 = Byte, %2 = Second Byte, %3 = high nibble of opcode #,
+// %4 = high nibble of opcode # and 07h, %5 = relative to PC+2
+// %6 = dp ($0/$1)
+// %7 = memory SHR 3 Low, %8 = memory SHR 3 High, %9 = ,memory AND 7h
+// %A = relative to PC+1, %B = relative to PC+1 at second byte
+char *AddressTable[68] = {
+//                  1                  1                  1
+"",                "%3",              "%6%1,%4",         "B%4 %6%1,$%B+1",
+// 0 : nothing
+// 1 : the high nibble
+// 2 : the high nibble first 3 bit (and 0111000 then shift)
+// 3 : 2 + relative
+"A,%6%1",          "A,$%2%1",         "A,(X)",           "A,(%6%1+x)",
+// 4 : A,dp
+// 5 : A,labs
+// 6 : A,(X)
+// 7 : A,(dp+X)
+"A,#$%1",          "(%6%2),(%6%1)",   "CF,mbit%8%7%9",   "%6%1",
+// 8 : A,#inm
+// 9 : dp(d),dp(s)   (two dp)
+// 10 : Carry flag, memory bit          (can only access from 0 to 1fff)
+// 11 : dp
+"$%2%1",           "PSW",             "$%A",             "A,%6%1+X",        
+// 12 : labs
+// 13 : PSW
+// 14 : rel
+// 15 : A,dp+X
+"A,$%2%1+X",       "A,$%2%1+Y",       "A,(%6%1)+Y",      "%6%2,#$%1",
+// 16 : A,labs+X
+// 17 : A,labs+Y
+// 18 : A,(dp)+Y
+// 19 : dp,#inm
+"(X),(Y)",         "%6%1+X",          "A",               "X",
+// 20 : (X),(Y)
+// 21 : dp+X
+// 22 : A
+// 23 : X
+"X,%2%1",          "($%2%1+X)",       "CF,/(mb%8%7%9)",  "%6%1",
+// 24 : X,labs
+// 25 : (labs+X)
+// 26 : C,/mem.bit
+// 27 : upage         (same as dp but for a call)
+"YA,%6%1",         "X,A",             "Y,$%2%1",         "Y",
+// 28 : YA,dp
+// 29 : X,A
+// 30 : Y,labs
+// 31 : Y
+"Y,%6%1",          "Y,#$%1",          "%6%1,$%B",        "X,%6%1",
+// 32 : Y,dp
+// 33 : Y,#inm
+// 34 : dp,rel
+// 35 : X,dp
+"A,X",             "%6%2,#$%1",       "X,SP",            "YA,X",
+// 36 : A,X
+// 37 : dp,#inm
+// 38 : X,SP
+// 39 : YA,X
+"(X)+,A",          "SP,X",            "A,(X)+",          "%6%1,A",
+// 40 : (X)+,A
+// 41 : SP,X
+// 42 : A,(X)+
+// 43 : dp,A
+"$%2%1,A",         "(X),A",           "%6%1+X,A",        "X,#$%1",
+// 44 : labs,A
+// 45 : (X),A
+// 46 : (dp+X),A
+// 47 : X,#inm
+"$%2%1,X",         "mb%8%7%9,CF",     "%6%1,Y",          "$%2%1,Y",
+// 48 : labs,X
+// 49 : mem.bit,C
+// 50 : dp,Y
+// 51 : labs,Y
+"YA",              "%6%1+X,A",        "$%2%1+X,A",       "$%2%1+Y,A",
+// 52 : YA
+// 53 : dp+X,A
+// 54 : labs+X,A
+// 55 : labs+Y,A
+"(%6%1)+Y,A",      "%6%1,X",          "%6%1+Y,X",        "%6%1,YA",
+// 56 : (dp)+Y,A
+// 57 : dp,X
+// 58 : dp+Y,X
+// 59 : dp,YA
+"%6%1+X,Y",        "A,Y",             "%6%2+X,$%A",      "mb%8%7%9,CF",
+// 60 : dp+X,Y
+// 61 : A,Y
+// 62 : dp+X,rel
+// 63 : mem.bit
+"X,%6%1+Y",        "Y,%6%1+X",        "Y,A",             "Y,$%A",
+// 64 : X,dp+Y
+// 65 : Y,dp+X
+// 66 : Y,A
+// 67 : Y,rel
+
+};
+
+unsigned char ArgumentTable[256] = {
+//     00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,12, 0,
+//     10 11 12 13 14 15 16 17 18 19 1A 1B 1C 1D 1E 1F
+       14, 1, 2, 3,15,16,17,18,19,20,11,21,22,23,24,25,
+//     20 21 22 23 24 25 26 27 28 29 2A 2B 2C 2D 2E 2F
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9,26,11,12,22,34,14,
+//     30 31 32 33 34 35 36 37 38 39 3A 3B 3C 3D 3E 3F
+       14, 1, 2, 3,15,16,17,18,19,20,11,21,22,23,35,12,
+//     40 41 42 43 44 45 46 47 48 49 4A 4B 4C 4D 4E 4F
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,23,12,27,
+//     50 51 52 53 54 55 56 57 58 59 5A 5B 5C 5D 5E 5F
+       14, 1, 2, 3,15,16,17,18,19,20,28,21,22,29,30,12,
+//     60 61 62 63 64 65 66 67 68 69 6A 6B 6C 6D 6E 6F
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9,26,11,12,31,34, 0,
+//     70 71 72 73 74 75 76 77 78 79 7A 7B 7C 7D 7E 7F
+       14, 1, 2, 3,15,16,17,18,19,20,28,21,22,36,32, 0,
+//     80 81 82 83 84 85 86 87 88 89 8A 8B 8C 8D 8E 8F
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,33,13,37,
+//     90 91 92 93 94 95 96 97 98 99 9A 9B 9C 9D 9E 9F
+       14, 1, 2, 3,15,16,17,18,19,20,28,21,22,38,39,22,
+//     A0 A1 A2 A3 A4 A5 A6 A7 A8 A9 AA AB AC AD AE AF
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,33,22,40,
+//     B0 B1 B2 B3 B4 B5 B6 B7 B8 B9 BA BB BC BD BE BF
+       14, 1, 2, 3,15,16,17,18,19,20,28,21,22,41,22,42,
+//     C0 C1 C2 C3 C4 C5 C6 C7 C8 C9 CA CB CC CD CE CF
+        0, 1, 2, 3,43,44,45,46,47,48,49,50,51,47,23,52,
+//     D0 D1 D2 D3 D4 D5 D6 D7 D8 D9 DA DB DC DD DE DF
+       14, 1, 2, 3,53,54,55,56,57,58,59,60,31,61,62,22,
+//     E0 E1 E2 E3 E4 E5 E6 E7 E8 E9 EA EB EC ED EE EF
+        0, 1, 2, 3, 4, 5, 6, 7, 8,24,63,32,30, 0,31, 0,
+//     F0 F1 F2 F3 F4 F5 F6 F7 F8 F9 FA FB FC FD FE FF
+       14, 1, 2, 3,15,16,17,18,35,64, 9,65,31,66,67, 0
+};
+
+
+// Jonas Quinn's file functions
 
 void SaveOAMRamLog() {
   FILE *fp = 0;
