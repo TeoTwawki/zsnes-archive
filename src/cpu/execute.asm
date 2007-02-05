82bb1b02d8daad83bc136fb705ed838b48ba105b
pagefault <pagefault@8cdf4848-9e13-0410-98e6-c9478ff1fb58>
2007-02-05 07:04:51 +0000

    Fixed some stuff
    git-svn-id: https://svn.zsnes.com/rep/zsnes/trunk@4588 8cdf4848-9e13-0410-98e6-c9478ff1fb58

--- src/cpu/execute.asm
+++ src/cpu/execute.asm
@@ -1699,8 +1699,6 @@ NEWSYM pexecs
    mov bl,[ebp]
    ; 1260, 10000/12625
    inc ebp
-   mov eax,[clocktable+ebx*4]
-   add dword[spcCycle],eax
    call dword near [opcjmptab+ebx*4]
    xor ebx,ebx
    dec byte[soundcycleft]
@@ -1716,8 +1714,6 @@ NEWSYM pexecs2
    mov bl,[ebp]
    ; 1260, 10000/12625
    inc ebp
-   mov eax,[clocktable+ebx*4]
-   add dword[spcCycle],eax
    call dword near [opcjmptab+ebx*4]
    xor ebx,ebx
    dec dword[soundcycleft]
