aa402074c172d38b782bae12aa0f01c55841b8a5
Deathlike2 <Deathlike2@8cdf4848-9e13-0410-98e6-c9478ff1fb58>
2007-02-09 22:02:48 +0000

    Dumb EXTSYM stupidity is not allowed in Windows+DOS.
    git-svn-id: https://svn.zsnes.com/rep/zsnes/trunk@4664 8cdf4848-9e13-0410-98e6-c9478ff1fb58

--- src/cpu/spc700.asm
+++ src/cpu/spc700.asm
@@ -21,10 +21,9 @@
 
 %include "macros.mac"
 
-EXTSYM DSPMem,disablespcclr,SPCSkipXtraROM,cycpbl
+EXTSYM DSPMem,disablespcclr,SPCSkipXtraROM,cycpbl,cycpblt
 EXTSYM spc700read,dspWptr,curexecstate,tableadc,opcjmptab
 EXTSYM DSP_val,DSP_reg,DSP_midframe,dsp_write_wrap,dsp_run_wrap
-EXTSYM cycpbl, cycpblt
 
 %include "cpu/regsw.mac"
 %include "cpu/spcdef.inc"
