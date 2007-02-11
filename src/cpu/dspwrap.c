f25338414d7e50dcc4e0fc06bcc61c6a8b34b531
Samuel Bronson <Samuel Bronson@8cdf4848-9e13-0410-98e6-c9478ff1fb58>
2007-02-11 04:51:20 +0000

    Prepare the way for blargg's SPC core (move spc.[ch] out of way, add new spc.h)
    git-svn-id: https://svn.zsnes.com/rep/zsnes/trunk@4677 8cdf4848-9e13-0410-98e6-c9478ff1fb58

--- src/cpu/dspwrap.c
+++ src/cpu/dspwrap.c
@@ -23,7 +23,7 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 #include <stdlib.h>
 #include <string.h>
 
-#include "spc.h"
+#include "dspwrap.h"
 #include "dsp.h"
 #include "../asm_call.h"
 
