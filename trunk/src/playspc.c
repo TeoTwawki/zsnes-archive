865ed5c1c210262ca4ac88edef18012acb600231
Samuel Bronson <Samuel Bronson@8cdf4848-9e13-0410-98e6-c9478ff1fb58>
2007-02-14 22:09:58 +0000

    Fix stupid; now SPC player works (for some games)
    git-svn-id: https://svn.zsnes.com/rep/zsnes@4712 8cdf4848-9e13-0410-98e6-c9478ff1fb58

--- trunk/src/playspc.c
+++ trunk/src/playspc.c
@@ -18,6 +18,8 @@ void (*opcjmptab[256])(void);
 
 extern unsigned char SPCRAM[];
 extern unsigned char* spcPCRam;
+extern unsigned char spcextraram[];
+extern unsigned char timincr0, timincr1, timincr2;
 extern unsigned int spcA, spcX, spcY, spcP, spcNZ, spcS;
 extern int DSP_midframe;
 extern void InitSPC();
@@ -32,13 +34,16 @@ int main(int argc, char *argv[]) {
     FILE *fin;
     unsigned char header[0x100];
     unsigned char DSPRegs[0x100];
+    unsigned char junk[0x40];
 
     InitSPC();
 
     fin = fopen(argv[1], "r");
     fread(header, 0x100, 1, fin);
-    fread(SPCRAM, 0x1000, 1, fin);
+    fread(SPCRAM, 0x10000, 1, fin);
     fread(DSPRegs, 0x80, 1, fin);
+    fread(junk, 0x40, 1, fin);
+    fread(spcextraram, 0x40, 1, fin);
 
     spcPCRam = SPCRAM+(header[0x25]+(header[0x26]<<8));
     spcA = header[0x27];
@@ -48,12 +53,23 @@ int main(int argc, char *argv[]) {
     spcNZ = -(header[0x2A]&2);
     spcS = 0x100+header[0x2B];
 
+    timincr0 = SPCRAM[0xfa];
+    timincr1 = SPCRAM[0xfb];
+    timincr2 = SPCRAM[0xfc];
+
     for (i = 0; i < 128; i++)
 	dsp_write(i, DSPRegs[i]);
 
     
     ao_initialize();
-    dev = ao_open_live(ao_default_driver_id(), &format, NULL);
+    dev = ao_open_live(ao_driver_id("oss"), &format, NULL);
+
+    if (dev) {
+	ao_info *di = ao_driver_info(dev->driver_id);
+	printf("\nAudio Opened.\nDriver: %s\nChannels: %u\nRate: %u\n\n", di->name, format.channels, format.rate);
+    } else {
+	exit(1);
+    }
 
     while (1) {
 	__asm__(
@@ -65,16 +81,17 @@ int main(int argc, char *argv[]) {
         popad               \n\
         .att_syntax"
 	    );
-	DSP_midframe = 0;
+	//DSP_midframe = 0;
 	dsp_run_wrap();
 	DSP_midframe = 1;
-	printf("PC = 0x%04x\n", spcPCRam-SPCRAM);
+	// printf("PC = 0x%04x\n", spcPCRam-SPCRAM);
     }
 }
 
 void SoundWrite_ao() {}
 
 void write_audio(short *sample_buffer, size_t sample_count) {
-    ao_play(dev, (char*)sample_buffer, sample_count*2);
+    if (!ao_play(dev, (char*)sample_buffer, sample_count*2))
+	exit(1);
 }
 
