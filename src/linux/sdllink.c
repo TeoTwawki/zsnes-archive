b93970a56c0e311fdb08c60a4763ebcdc77e85a1
Deathlike2 <Deathlike2@8cdf4848-9e13-0410-98e6-c9478ff1fb58>
2007-02-23 00:23:43 +0000

    Temporary "fix" for OSX users and ATI cards. Variable mode resizing is disabled for OSX users. Thanks to Krade for testing. Now go bother Apple, SDL dev, and ATI for why it does not work normally.
    git-svn-id: https://svn.zsnes.com/rep/zsnes/trunk@4805 8cdf4848-9e13-0410-98e6-c9478ff1fb58

--- src/linux/sdllink.c
+++ src/linux/sdllink.c
@@ -417,15 +417,12 @@ int Main_Proc(void)
       case SDL_QUIT:
         exit(0);
         break;
+#ifndef __MACOSX__
 #ifdef __OPENGL__
       case SDL_VIDEORESIZE:
         if(!GUIRESIZE[cvidmode])
         {
-          SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-#if (SDL_MAJOR_VERSION > 1) || ((SDL_MINOR_VERSION > 2) || ((SDL_MINOR_VERSION == 2) && (SDL_PATCHLEVEL >= 10)))
-          SDL_GL_SetAttribute(SDL_GL_SWAP_CONTROL, 1);
-          SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL,1);
-#endif
+          SetGLAttributes();
           surface = SDL_SetVideoMode(WindowWidth, WindowHeight, BitDepth, surface->flags & ~SDL_RESIZABLE);
           adjustMouseXScale();
           adjustMouseYScale();
@@ -435,11 +432,7 @@ int Main_Proc(void)
         WindowHeight = SurfaceY = event.resize.h;
         SetHQx(SurfaceX,SurfaceY);
         SetHiresOpt(SurfaceX,SurfaceY);
-        SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-#if (SDL_MAJOR_VERSION > 1) || ((SDL_MINOR_VERSION > 2) || ((SDL_MINOR_VERSION == 2) && (SDL_PATCHLEVEL >= 10)))
-        SDL_GL_SetAttribute(SDL_GL_SWAP_CONTROL, 1);
-        SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL,1);
-#endif
+        SetGLAttributes();
         surface = SDL_SetVideoMode(WindowWidth, WindowHeight, BitDepth, surface->flags);
         adjustMouseXScale();
         adjustMouseYScale();
@@ -490,6 +483,7 @@ int Main_Proc(void)
         gl_clearwin();
         Clear2xSaIBuffer();
         break;
+#endif
 #endif
       default:
         break;
@@ -1030,11 +1024,7 @@ void initwinvideo(void)
     #ifdef __OPENGL__
     if(OGLModeCheck())
     {
-      SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-#if (SDL_MAJOR_VERSION > 1) || ((SDL_MINOR_VERSION > 2) || ((SDL_MINOR_VERSION == 2) && (SDL_PATCHLEVEL >= 10)))
-      SDL_GL_SetAttribute(SDL_GL_SWAP_CONTROL, 1);
-      SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL,1);
-#endif
+      SetGLAttributes();
       surface = SDL_SetVideoMode(WindowWidth, WindowHeight, BitDepth, surface->flags);
       adjustMouseXScale();
       adjustMouseYScale();
@@ -1082,6 +1072,11 @@ void initwinvideo(void)
       glLoadIdentity();
       glDisable(GL_DEPTH_TEST);
       glFlush();
+      #ifdef __MACOSX__
+      clearwin();
+      Clear2xSaIBuffer();
+      initwinvideo();		//really really bad hack for OSX+ATI video cards -DL
+      #endif
     }
     #endif
     clearwin();
