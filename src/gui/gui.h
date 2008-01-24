e5a1ca97846f2c8385b19e570e68f3cd107f1580
pagefault <pagefault@8cdf4848-9e13-0410-98e6-c9478ff1fb58>
2008-01-24 17:09:31 +0000

    Here comes the QT gui
    git-svn-id: https://svn.zsnes.com/rep/zsnes/trunk@5193 8cdf4848-9e13-0410-98e6-c9478ff1fb58

---
+++ src/gui/gui.h
@@ -0,0 +1,14 @@
+#include "ui_zsnes.h"
+
+class ZSNESMainForm : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    ZSNESMainForm(QMainWindow *parent = 0);
+
+private slots:
+ 
+private:
+    Ui::ZSNESMainForm ui;
+ }; 
