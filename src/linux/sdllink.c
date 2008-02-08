fe275a79cbea9998bdf229c0beebc567809e88a6
Nach <Nach@8cdf4848-9e13-0410-98e6-c9478ff1fb58>
2008-02-08 09:57:36 +0000

    Trick screensavers into keys being pressed for SDL, so gamepad users don't have their game interrupted.
    git-svn-id: https://svn.zsnes.com/rep/zsnes/trunk@5201 8cdf4848-9e13-0410-98e6-c9478ff1fb58

--- src/linux/sdllink.c
+++ src/linux/sdllink.c
@@ -29,6 +29,7 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 #include <sys/time.h>
 #include <time.h>
 
+#include "x11.h"
 #include "audio.h"
 #include "safelib.h"
 #include "../cfg.h"
@@ -540,6 +541,8 @@ int Main_Proc()
     }
   }
 
+  CircumventXScreenSaver();
+
   return TRUE;
 }
 
@@ -1028,6 +1031,8 @@ int startgame()
     return FALSE;
   }
   sdl_state = (UseOpenGL ? vid_gl : vid_soft);
+
+  X11_Init();
 
   return TRUE;
 }
