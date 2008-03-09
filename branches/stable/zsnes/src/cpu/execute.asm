401113f843c0fec990acf9137bbb828e55440afd
jbo_85 <jbo_85@8cdf4848-9e13-0410-98e6-c9478ff1fb58>
2008-03-09 03:29:00 +0000

    Backported RTO and some other sprite fixes to the stable branch (revisions 4837, 4984, 5216, 5217).
    git-svn-id: https://svn.zsnes.com/rep/zsnes@5218 8cdf4848-9e13-0410-98e6-c9478ff1fb58

--- branches/stable/zsnes/src/cpu/execute.asm
+++ branches/stable/zsnes/src/cpu/execute.asm
@@ -53,6 +53,7 @@ EXTSYM MovieSeekBehind,BackupCVFrame,RestoreCVFrame,loadstate,xe
 EXTSYM KeyInsrtChap,KeyNextChap,KeyPrevChap,MovieInsertChapter,MovieSeekAhead
 EXTSYM ResetDuringMovie,EMUPauseKey,INCRFrameKey,MovieWaiting,NoInputRead
 EXTSYM AllocatedRewindStates,PauseFrameMode,RestorePauseFrame,BackupPauseFrame
+EXTSYM rtoflags,sprcnt,sprstart,sprtilecnt,sprend,sprendx
 
 %ifndef NO_DEBUGGER
 EXTSYM debuggeron,startdebugger
@@ -722,6 +723,11 @@ Donextlinecache:
 .next
     mov byte[sprlefttot+ecx],0
     mov dword[sprleftpr+ecx*4],0
+    mov byte[sprcnt+ecx],0
+    mov byte[sprstart+ecx],0
+    mov byte[sprtilecnt+ecx],0
+    mov byte[sprend+ecx],0
+    mov word[sprendx+ecx*2],0
     inc cl
     jnz .next
     call processsprites
@@ -858,6 +864,10 @@ NEWSYM NoHDMALine, db 0
 SECTION .text
 
 NEWSYM cpuover
+    cmp word[curypos],0
+    jne .nortoreset
+    mov byte[rtoflags],0
+.nortoreset
     dec esi
     cmp byte[HIRQNextExe],0
     je .nohirq
@@ -1051,6 +1061,18 @@ NEWSYM cpuover
 .returnfromsfx
 ;    inc dword[numinst]          ;Temporary
     inc word[curypos]
+    xor eax,eax
+    mov ax,[curypos]
+    cmp ax,[resolutn]
+    ja .norangeover
+    cmp byte[sprtilecnt+eax],34
+    jbe .notimeover
+    or byte[rtoflags],80h
+.notimeover
+    cmp byte[sprcnt+eax],32
+    jbe .norangeover
+    or byte[rtoflags],40h
+.norangeover
     add dh,[cycpl]
     mov ax,[totlines]
     cmp word[curypos],ax
